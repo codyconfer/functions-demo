f4eb9552b5bbacc9e12dcd2cc164176b9478267f
codyconfer <mail@codyconfer.me>
2019-10-01 17:06:21 -0600

    add redis connection logic

--- api/src/mutate/Services/RoomService.cs
+++ api/src/mutate/Services/RoomService.cs
@@ -1,13 +1,27 @@
 using System;
 using FunctionsDemo.Mutate.Models.Room;
+using StackExchange.Redis;
 
 namespace FunctionsDemo.Mutate.Services
 {
     public class RoomService
     {
+        private readonly Lazy<ConnectionMultiplexer> _lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            var connectionString = Environment.GetEnvironmentVariable("RedisConnection");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("RedisConnection must be populated in app settings");
+            }
+
+            return ConnectionMultiplexer.Connect(connectionString);
+        });
+
+        private ConnectionMultiplexer RedisConnection => _lazyConnection.Value;
+
         public MessageAddResponse AddMessage(MessageAddRequest request)
         {
-            return new MessageAddResponse 
+            return new MessageAddResponse
             {
                 MessageId = 0,
                 Timestamp = DateTimeOffset.Now
--- api/src/query/Services/RoomService.cs
+++ api/src/query/Services/RoomService.cs
@@ -1,11 +1,25 @@
 using System;
 using System.Collections.Generic;
 using FunctionsDemo.Query.Models.Room;
+using StackExchange.Redis;
 
 namespace FunctionsDemo.Query.Services.Room
 {
     public class RoomService
     {
+        private readonly Lazy<ConnectionMultiplexer> _lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            var connectionString = Environment.GetEnvironmentVariable("RedisConnection");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("RedisConnection must be populated in app settings");
+            }
+
+            return ConnectionMultiplexer.Connect(connectionString);
+        });
+
+        private ConnectionMultiplexer RedisConnection => _lazyConnection.Value;
+
         public MessagesResponse GetMessages(int roomId) 
         {
             return new MessagesResponse()
